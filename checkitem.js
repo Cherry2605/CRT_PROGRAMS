794b7c8ae1d179a8d6a1611b49d93496b2c8ecb5
Cherry2605 <charancherry9704428@gmail.com>
2026-03-13 11:43:18 +0530

    second commit

---
+++ checkitem.js
@@ -0,0 +1,4 @@
+function checkitem(arr,item){
+    return arr.includes(item);
+}
+console.log(checkitem(["pencil","eraser","notebook"],"eraser"));
---
+++ reversearray.js
@@ -0,0 +1,4 @@
+function reversearray(arr){
+    return arr.reverse();
+}
+console.log(reversearray([1,2,3,4,5]));
